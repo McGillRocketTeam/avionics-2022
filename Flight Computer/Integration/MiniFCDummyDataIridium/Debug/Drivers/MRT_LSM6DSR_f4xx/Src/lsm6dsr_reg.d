6e75a3254b66984cff4d4118c0c6f9c4d8aa0323
TahaLazreg <tlazreg1@gmail.com>
2022-02-02 16:49:17 -0500

    Added new FC

---
+++ Flight Computer/Integration/MiniFCDummyDataIridium/Debug/Drivers/MRT_LSM6DSR_f4xx/Src/lsm6dsr_reg.d
@@ -0,0 +1,5 @@
+Drivers/MRT_LSM6DSR_f4xx/Src/lsm6dsr_reg.o: \
+ ../Drivers/MRT_LSM6DSR_f4xx/Src/lsm6dsr_reg.c \
+ ../Drivers/MRT_LSM6DSR_f4xx/Inc/lsm6dsr_reg.h
+
+../Drivers/MRT_LSM6DSR_f4xx/Inc/lsm6dsr_reg.h:
