13feb44df9fc3281f209c000ab06e7e815e0c45e
salimei <salim.benchekroun@mail.mcgill.ca>
2022-02-07 19:57:29 -0500

    beware, skeleton created!!!

--- Arrays/main.py
+++ Arrays/main.py
@@ -1,4 +1,10 @@
 
+import matplotlib.pyplot as plt
+
+"""
+This file's main purpose is testing. 
+
+"""
 
 def PlotKF(time, position, ylabel, col): #Plots KF
     plt.scatter(time, position, color=col, linestyle='--', linewidths=0.1, 
@@ -6,3 +12,6 @@ def PlotKF(time, position, ylabel, col): #Plots KF
     plt.xlabel('time')
     plt.ylabel(ylabel)
     
+#create instance 
+
+#while loop 
