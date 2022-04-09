ea4c9bc5c03afcce6c70ba51aea9b6ba2d7610c4
salimei <salim.benchekroun@mail.mcgill.ca>
2022-04-09 16:40:05 -0400

    MEKF to C #1
    -File structure established

---
+++ Arrays/MekfLib/Inc/mekf.h
@@ -0,0 +1,10 @@
+
+#ifndef MEKF_H_
+#define MEKF_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#endif 
---
+++ Arrays/MekfLib/main.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Inc/mekf.h"
+
+
+int main(void) {
+
+    
+	printf(" %s \n", "test");
+
+
+
+	return(0);
+}
