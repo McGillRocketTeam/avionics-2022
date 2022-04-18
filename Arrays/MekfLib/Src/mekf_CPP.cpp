5f3532c35ef6352a0fbbe4e3734230532715d039
salimei <salim.benchekroun@mail.mcgill.ca>
2022-04-18 15:03:33 -0400

    MEKF to C #3
    -Eigen library successfully added

--- Arrays/MekfLib/Src/mekf_CPP.cpp
+++ Arrays/MekfLib/Src/mekf_CPP.cpp
@@ -3,13 +3,19 @@
 #include <string.h>
 #include <iostream>
 #include "../Inc/mekf_CPP.h"
+#include "C:/Users/Dell/CPP_libraries/eigen-3.4.0/Eigen/Dense"
 
 MEKF::MEKF() { //user defined default constructor
     this->a = 3;
 }
 
 void MEKF::kf_predict(){
+    Eigen::Matrix3d r;
+    r << 1, 0, 0,
+         0, 1, 0,
+         0, 0, 1;
     std::cout << "cpp predict" << std::endl;
+    std::cout << r << std::endl;
 }
 
 void MEKF::kf_correct() {
